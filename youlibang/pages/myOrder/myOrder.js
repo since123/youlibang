8a7c43bdbc77639fc15630eb1aa89acbafbe172c
tanxuejiao <1534147975@qq.com>
2019-07-17 22:10:24 +0800

    fix myorder swider height error

--- youlibang/pages/myOrder/myOrder.js
+++ youlibang/pages/myOrder/myOrder.js
@@ -7,7 +7,8 @@ Page({
   data: {
     currtab: 0,
     swipertab: [{ name: '全部', index: 0 }, { name: '待付款', index: 1 }, { name: '待发货', index: 2 }, { name: '待收货', index: 3 }, { name: '已完成', index: 4 }],
-    waitPayOrder: [{ index: '0', image: '/images/2012031220134655.jpg', title: 'Pepe Jeans秋冬新款女士长袖连衣裙', color: '黑色', size: 'L', unit: '件', price: '120', number: '8'}]
+    waitPayOrder: [{ index: '0', image: '/images/2012031220134655.jpg', title: 'Pepe Jeans秋冬新款女士长袖连衣裙', color: '黑色', size: 'L', unit: '件', price: '120', number: '8'}],
+    height: 0
   },
 
   /**
@@ -21,7 +22,15 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
+    function get_wxml(className, callback) {
+      wx.createSelectorQuery().select(className).boundingClientRect(callback).exec()
+    }
 
+    get_wxml('.container-0', rect => {
+      const height = rect.height
+      this.setData({ height:  height })
+      console.log('height', height)
+    })
   },
   /**
     * @Explain：选项卡点击切换
@@ -31,10 +40,29 @@ Page({
     if (this.data.currtab === e.target.dataset.current) {
       return false
     } else {
-      that.setData({
-        currtab: e.target.dataset.current
+      function get_wxml(className, callback) {
+        wx.createSelectorQuery().select(className).boundingClientRect(callback).exec()
+      }
+
+      let tab = e.target.dataset.current
+
+      get_wxml('.container-' + tab, rect => {
+        const height = rect.height
+
+        that.setData({
+          height: height,
+          currtab: tab
+        })
+        console.log('height', height, e.target.dataset.current)
       })
+
+
+    
     }
+  },
+
+  onTabChange(e) {
+    console.log('change', e)
   },
   /**
    * 生命周期函数--监听页面显示
